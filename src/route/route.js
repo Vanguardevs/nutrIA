a124c698829c7d787d26731f91fd1e9ffef07f61
Kyra4code <lucarioecharmito@gmail.com>
2024-11-17 13:47:12 -0300

    O pacote nao esta baixando, porem acredito que esta tudo ok

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,36 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Wellcome from '../pages/wellcome/welcome.js';
 import LoginPag from "../pages/loginUser/login.js";
 import createUser from "../pages/loginUser/register.js";
-import appTab from "./tabsLogin.js";
+import Home from "../pages/Main/Home.js";
+import Settings from "../pages/Main/Config.js";
 
 const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+function appTab(){
+    return(
+    <Tabs.Navigator
+    initialRouteName="Home"
+    screenOptions={{headerShown: false}}
+    >
+        <Tabs.Screen
+            name="Home"
+            component={Home}
+            options={{title: 'Chat', tabBarIcon:({size,color})=>(
+                <MaterialCommunityIcons name="Home" size={size} color={color}/>
+            )
+        }}
+        />
+        <Tabs.Screen
+            name="Settings"
+            component={Settings}
+        />
+    </Tabs.Navigator>
+    )
+}
 
 export default function RoutePag(){
     return(
