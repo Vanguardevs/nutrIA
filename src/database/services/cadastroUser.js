551ecafc1a8523b10639ffce6ef220b5b3ad9802
Kyra4code <lucarioecharmito@gmail.com>
2025-03-02 15:40:01 -0300

    Foi criado funcionalidades disponíveis de cadastrar o usuário direto com o banco

--- src/database/services/cadastroUser.js
+++ src/database/services/cadastroUser.js
@@ -12,14 +12,22 @@ const cliente = new Client({
 
 cliente.connect()
 
-async function CadastroUser(){
+async function CadastroUser(nome, email, senha, idade, sexo, type_user){
     try{
-        const response = await cliente.query("insert into tbl_usuario values (?,?,?,?,?,?)")
+        await cliente.query(`insert into usuarios values (${nome}, ${email}, ${senha}, ${idade}, ${sexo}, ${type_user})`)
         console.log("Insert na tabela de usuário feito com sucesso!")
     }catch(error){
         console.log("Erro: " + error)
     }
 }
 
+async function CadastroHiSaude(peso_n, gordura_corporal, massa_musc, taxa_metabo, ingestao_calorica_di, ){
+    try{
+        await cliente.query(`insert into historico_saude values (${peso_n}, ${gordura_corporal}, ${massa_musc}, ${taxa_metabo}, ${ingestao_calorica_di})`)
+        console.log("Insert na tabela de histórico de saude do usuário feito com sucesso!");
+    }catch(errror){
+        console.log("Erro: " + errror)
+    }
+}
 
 export default {CadastroUser}
